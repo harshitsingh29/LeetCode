69e2ddcce12f33cdbf217eade48ebb94888c4578
Harshit Singh <harsh.official29@gmail.com>
2022-09-11 16:21:51 +0530

    Time: 179 ms (25.00%), Space: 36.9 MB (34.51%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
@@ -11,7 +11,7 @@ public:
         long long int gsum = 0;
         long long int res = 0;
         for(auto& [x, y] : vpair) {
-            cout<<x<<" "<<y<<endl;
+            // cout<<x<<" "<<y<<endl;
             pq.push(y);
             gsum += y;
             if(pq.size() > k) {
