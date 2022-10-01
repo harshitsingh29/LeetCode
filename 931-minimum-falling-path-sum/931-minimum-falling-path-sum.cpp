efac11e33f459ef1d094ac0d62eb46b9fc5cb522
Harshit Singh <harsh.official29@gmail.com>
2022-10-02 02:32:32 +0530

    Time: 19 ms (76.93%), Space: 10.4 MB (6.15%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -18,9 +18,35 @@ public:
         int ans = INT_MAX;
         int n = m.size();
         int mm = m[0].size();
-        vector<vector<int>>dp(n, vector<int>(mm,-1));
-        for(int i = 0; i < m[0].size(); i++) {
-            ans = min(ans, f(0,i,m,dp));
+        vector<vector<int>>dp(n, vector<int>(mm,1e7));
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < mm; j++) {
+                if(i == 0) dp[i][j] = m[i][j];
+                else {
+                    int l, r;
+                    if(j > 0) {
+                        l = m[i][j] + dp[i-1][j-1];
+                    }
+                    else {
+                        l = 1e7;
+                    }
+                    
+                    if(j < mm-1) {
+                        r = m[i][j] + dp[i-1][j+1];
+                    }
+                    else {
+                        r = 1e7;
+                    }
+                    
+                    int u = m[i][j] + dp[i-1][j];
+                    
+                    dp[i][j] = min({u,l,r});
+                    
+                }
+            }
+        }
+        for(int j = 0; j < mm; j++) {
+            ans = min(ans, dp[n-1][j]);
         }
         return ans;
     }
