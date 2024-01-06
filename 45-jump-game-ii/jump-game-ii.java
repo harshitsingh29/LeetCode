c22fed60ca012da9ac448eadc34aefac96eaa4b3
Harshit Singh <harsh.official29@gmail.com>
2024-01-06 17:56:50 +0530

    Time: 2 ms (70.32%) | Memory: 45.2 MB (25.55%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -10,21 +10,18 @@ class Solution {
             return 1;
         }
         while(i < n) {
-            //int k = nums[i];
             int e = Math.min(nums[i]+i+1, n);
             int max = 0, newi = i+1;
             count++;
             for(int j = i+1; j < e; j++) {
                 if(nums[j] != 0) {
                     if(j + nums[j] >= n-1) {
-                        //System.out.println(nums)
                         return count;
                     }
                     else {
                         if(j + nums[j] > max) {
                             max = j + nums[j];
                             i = j;
-                            System.out.println(newi);
                         }
                         else {
                             continue;
@@ -32,7 +29,6 @@ class Solution {
                     }
                 }
             }
-            //i = newi;
         }
         return count;
     }
