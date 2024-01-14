b86b5ea92056a3da79435e3f61b6ed96c649686b
Harshit Singh <harsh.official29@gmail.com>
2024-01-14 11:52:43 +0530

    Time: 2 ms (96.92%) | Memory: 44.1 MB (43.51%) - LeetSync

---
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) {
+            return true;
+        }
+        if(p == null || q == null || p.val != q.val) {
+            return false;
+        }
+        return (isSameTree(p.left,q.left) && isSameTree(p.right, q.right));
+    }
+
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root == null) {
+            return false;
+        }
+        if(root.val != subRoot.val) {
+            boolean left = isSubtree(root.left, subRoot);
+            boolean right = isSubtree(root.right, subRoot);
+            return left || right;
+        }
+        else {
+            boolean ans = isSameTree(root, subRoot);
+            if(ans == true) {
+                return true;
+            }
+            else {
+                boolean left = isSubtree(root.left, subRoot);
+                boolean right = isSubtree(root.right, subRoot);
+                return left || right;
+            }
+        }
+    }
+}
